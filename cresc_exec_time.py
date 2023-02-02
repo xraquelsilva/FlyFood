85f8a389f8e363edfb0e5f39f3ef90b1528ebc14
Raquel Silva <105558085+xraquelsilva@users.noreply.github.com>
2023-02-02 16:06:44 -0300

    Delete cresc_exec_time.py

--- cresc_exec_time.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot
-
-inst = ['2', '3', '4', '5', '6', '7', '8','9']
-time = [0, 0, 0.002, 0.002, 0.042, 3.69, 140.1, 425]
-
-matplotlib.pyplot.plot(inst, time)
-matplotlib.pyplot.show()
